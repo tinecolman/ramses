c172e90574db2eacb7cb3440270916f824fd4b83
Tine Colman <tinecolman@hotmail.com>
2025-02-17 16:48:56 +0100

    plot evolution of execution time

--- benchmarks/analyse_benchmark.py
+++ benchmarks/analyse_benchmark.py
@@ -31,6 +31,8 @@ import numpy as np
 import os
 import re
 from matplotlib import pyplot as plt
+import matplotlib.colors as colorsx
+import matplotlib.cm as cmx
 
 ''' Get average time and error bars from total time printed in log files, for a certain resolution-node config '''
 def get_timings_total(run_dir):
@@ -38,11 +40,15 @@ def get_timings_total(run_dir):
     subprocess.call("grep --no-filename 'Total elapsed time' {}/*.log".format(run_dir) +" | awk '{print $4}' > total_time.txt", shell=True)
     total_time=np.loadtxt('total_time.txt', unpack=True)
     total_time=np.array([total_time]).flatten()
-    print(total_time)
-    # take the average and determine the error
-    time = np.sum(total_time) / len(total_time)
-    error_min = time-np.min(total_time)
-    error_max = np.max(total_time)-time
+    if len(total_time)>0:
+        # take the average and determine the error
+        time = np.sum(total_time) / len(total_time)
+        error_min = time-np.min(total_time)
+        error_max = np.max(total_time)-time
+    else:
+        time = np.nan
+        error_min=0
+        error_max=0
     return time, error_min, error_max
 
 
@@ -93,26 +99,43 @@ def plot_strong_scaling(benchmark_dir, reso_strong, nodes_strong):
 
 
 '''  '''
-def plot_execution_time(benchmark_dir, reso_strong, nodes_strong):
+def plot_execution_time(benchmark_dir_list, reso_strong, nodes_strong):
 
-    # gather data from log files
-    times, errors_min, errors_max = [], [], []
-    for nnodes in nodes_strong:
-        #subdir_name = 'nodes'+str(nnodes)+'_reso'+str(reso_strong)
-        subdir_name = 'nodes'+str(nnodes)+'_threads8'
-        time, error_min, error_max = get_timings_total(benchmark_dir+'/'+subdir_name)
-        times.append(time)
-        errors_min.append(error_min)
-        errors_max.append(error_max)
-    asymmetric_error=np.array(list(zip(errors_min, errors_max))).T
+    dates = []
+    times = {}
+    errors_min = {}
+    errors_max = {}
+    for n in nodes_strong:
+        times[n] = []
+        errors_min[n] = []
+        errors_max[n] = []
 
-    # make strong scaling plot
+    for benchmark_dir in benchmark_dir_list:
+        #dates.append(benchmark_dir[-16:-6])
+        dates.append(benchmark_dir[-25:-17]+'\n'+benchmark_dir[-16:-6])
+        # gather data from log files
+        for n in nodes_strong:
+            subdir_name = 'nodes'+str(n)+'_reso'+str(reso_strong)
+            time, error_min, error_max = get_timings_total(benchmark_dir+'/'+subdir_name)
+            times[n].append(time)
+            errors_min[n].append(error_min)
+            errors_max[n].append(error_max)
+
+    # create colors
+    cmap = cmx.get_cmap('managua')
+    cNorm  = colorsx.Normalize(vmin=0, vmax=len(nodes_strong)-1)
+    colorVals =  []
+    for val in range(len(nodes_strong)):
+        colorVals.append(cmap(cNorm(val)))
+
+    # plot
     plt.figure(figsize=[5,4])
-    dates = ['today']
-    for i in range(len(nodes_strong)):
-        print(dates, [times[i]], [[errors_min[i]],[errors_max[i]]])
-        plt.errorbar(dates, [times[i]], yerr=[[errors_min[i]],[errors_max[i]]], fmt='o', markersize=3, label=str(nodes_strong[i])+' nodes')
-    
+    for i, c in zip(nodes_strong,colorVals):
+        plt.errorbar(dates, times[i], yerr=[errors_min[i],errors_max[i]], fmt='o', markersize=5,
+                     label=str(i)+' nodes', color=c)
+        # plot a line from the last point to make comparison easier
+        plt.plot([dates[0],dates[-1]], [times[i][-1],times[i][-1]], ls=':', lw=1.3, color=c)
+
     plt.ylabel('execution time [s]')
     plt.yscale('log')
     plt.legend()
@@ -121,7 +144,11 @@ def plot_execution_time(benchmark_dir, reso_strong, nodes_strong):
 
 
 if __name__ == '__main__':
-    benchmark_dir = '/home/tcolman/Dropbox/SPACE/benchmarks/meluxina/tests_openmp_3692b03d_sedov1024'
+
+    bench_home = '/home/tcolman/Dropbox/SPACE/benchmarks/marenostrum/'
+    test = 'sedov'
+    benchmark_dir_list = [bench_home+'benchmark_performance_tests_24fe23ee_2025-02-17/'+test,
+                          bench_home+'benchmark_performance_tests_b5104a59_2025-02-17/'+test]
     reso_strong = 1024
-    nodes_strong = [4,8,16,32,64]
-    plot_execution_time(benchmark_dir, reso_strong, nodes_strong)
+    nodes_strong = [1,2,4,8,16,32,64]
+    plot_execution_time(benchmark_dir_list, reso_strong, nodes_strong)
