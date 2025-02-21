70925b9bd881bb29fa3f1063a1f0b90100c179d1
Tine Colman <tine.colman@ens-lyon.fr>
2025-02-21 16:43:12 +0100

    improve gathering results

--- benchmarks/HPCclusters/meluxina/gather_results.sh
+++ benchmarks/HPCclusters/meluxina/gather_results.sh
@@ -1,36 +1,33 @@
-# ------------------- Construct job script ------------
+#!/bin/bash
 
-JOB_FILE=io_${TEST_NAME}.sh
+# Define output file
+OUTPUT_FILE="io_${TEST_NAME}.sh"
 
-echo "#!/bin/bash -l" > ${JOB_FILE}
+# ------------------- Construct job script that will analyse the data ------------
 
-# SLURM job settings
-echo "#SBATCH --job-name=io-${TEST_NAME}" >> ${JOB_FILE}
-echo "#SBATCH --account=${CLUSTER_ALLOCATION_ID}" >> ${JOB_FILE}
-echo "#SBATCH --partition=${CLUSTER_PARTITION}" >> ${JOB_FILE}
-echo "#SBATCH --qos=${CLUSTER_QOS}" >> ${JOB_FILE}
-echo "#SBATCH --nodes=1" >> ${JOB_FILE}
-echo "#SBATCH --ntasks-per-node=1" >> ${JOB_FILE}
-echo "#SBATCH --cpus-per-task=1" >> ${JOB_FILE}
-echo "#SBATCH --time=00:05:00" >> ${JOB_FILE}
-echo "#SBATCH --output=slurm_${TEST_NAME}.out" >> ${JOB_FILE}
-echo "#SBATCH --error=slurm_${TEST_NAME}.err" >> ${JOB_FILE}
+cat <<COMPILEJOB > "$OUTPUT_FILE"
+#!/bin/bash -l
+#SBATCH --job-name=io-${TEST_NAME}
+#SBATCH --account=${CLUSTER_ALLOCATION_ID}
+#SBATCH --partition=${CLUSTER_PARTITION}
+#SBATCH --qos=${CLUSTER_QOS}
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:05:00
+#SBATCH --output=slurm_${TEST_NAME}.out
+#SBATCH --error=slurm_${TEST_NAME}.err
 
-echo "" >> ${JOB_FILE}
+module load ${MODULE_PYTHON}
 
-# modules
-echo "module load ${MODULE_PYTHON}" >> ${JOB_FILE}
-
-echo "" >> ${JOB_FILE}
-
-# run command
-echo "python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} >> ${LOGFILE} " >> ${JOB_FILE}
+python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} >> ${LOGFILE} 
 
 # clean up
-echo "rm total_time.txt" >> ${JOB_FILE}
+rm total_time.txt
 
 # push to git
-echo "git add timings_${CLUSTER}_${TEST_NAME}.txt" >> ${JOB_FILE}
-echo "git commit -m 'update benchmark ${CLUSTER} ${TEST_NAME}'" >> ${JOB_FILE}
-echo "git push" >> ${JOB_FILE}
+git add results/timings_${CLUSTER}_${TEST_NAME}.txt
+git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
+#git push
 
+COMPILEJOB
