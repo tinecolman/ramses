eae1d8549e6e27071662f4bc38c00778093b7daf
Tine Colman <tinecolman@hotmail.com>
2025-03-04 14:25:09 +0200

    don't auto commit for now

--- benchmarks/io_job.sh
+++ benchmarks/io_job.sh
@@ -9,7 +9,7 @@ python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} >> ${LOGFILE}
 rm total_time.txt
 
 # push to git
-git add results/timings_${CLUSTER}_${TEST_NAME}.txt
-git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
+#git add results/timings_${CLUSTER}_${TEST_NAME}.txt
+#git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
 #git push
 JOBSCRIPT
