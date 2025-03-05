79c3a3d5294c4706a7e54c9516920968e67f7a65
Tine Colman <tine.colman@ens-lyon.fr>
2025-03-05 17:19:44 +0100

    fix submodule

--- benchmarks/io_job.sh
+++ benchmarks/io_job.sh
@@ -2,6 +2,19 @@
 
 # append commands
 cat <<JOBSCRIPT >> "$OUTPUT_FILE"
+
+#######################################################################
+# Get the existing results
+#######################################################################
+
+eval "$(ssh-agent -s)"
+cd ..
+git submodule update --init --recursive
+cd benchmarks/results
+git checkout main
+git pull origin main
+cd ..
+
 module load ${MODULE_PYTHON}
 python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} >> ${LOGFILE}
 
@@ -10,7 +23,7 @@ rm total_time.txt
 
 # push to git
 cd results
-git add timings_${CLUSTER}_${TEST_NAME}.txt
+git add data/timings_${CLUSTER}_${TEST_NAME}.txt
 git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
 git push
 cd ..
