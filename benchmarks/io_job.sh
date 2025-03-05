0f5f215ca22b49cfbf1bbb993f0823ba3b0fd93b
Tine Colman <tinecolman@hotmail.com>
2025-03-05 16:25:07 +0100

    auto push timings

--- benchmarks/io_job.sh
+++ benchmarks/io_job.sh
@@ -9,7 +9,9 @@ python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} >> ${LOGFILE}
 rm total_time.txt
 
 # push to git
-#git add results/timings_${CLUSTER}_${TEST_NAME}.txt
-#git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
-#git push
+cd results
+git add timings_${CLUSTER}_${TEST_NAME}.txt
+git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
+git push
+cd ..
 JOBSCRIPT
