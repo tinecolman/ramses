c03a36ca4da36403705ee96f32c319229d61a14d
Tine Colman <tinecolman@hotmail.com>
2025-03-17 15:13:21 +0100

    different subdirs for different branches

--- benchmarks/io_job.sh
+++ benchmarks/io_job.sh
@@ -13,16 +13,17 @@ git submodule update --init --recursive
 cd benchmarks/results
 git checkout main
 git pull origin main
+mkdir -p data_${BRANCH}
 
 module load ${MODULE_PYTHON}
-python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} >> ${LOGFILE}
+python io_timings.py ${CLUSTER} ${LAUNCH_DIR} ${TEST_NAME} ${BRANCH} >> ${LOGFILE}
 
 # clean up
 rm total_time.txt
 
 # push to git
-git add data/timings_${CLUSTER}_${TEST_NAME}.txt
-git commit -m 'update benchmark results ${CLUSTER} ${TEST_NAME}'
+git add data_${BRANCH}/timings_${CLUSTER}_${TEST_NAME}.txt
+git commit -m 'update ${TEST_NAME} benchmark results of branch ${BRANCH} on ${CLUSTER}'
 git push
 cd ..
 JOBSCRIPT
