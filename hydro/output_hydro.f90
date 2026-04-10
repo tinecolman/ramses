f0ef035cbe8b31166d8e5ac73af33ec8a51d609d
Tine Colman <tinecolman@hotmail.com>
2026-04-10 12:03:00 +0200

    simplify outputting of passive scalars

--- hydro/output_hydro.f90
+++ hydro/output_hydro.f90
@@ -174,10 +174,13 @@ subroutine backup_hydro(filename, filename_desc)
               end if
               call generic_dump(field_name, info_var_count, xdp, unit_out, dump_info_flag, unit_info)
 
+#if NVAR > NHYDRO+NENER
               ! Write passive scalars if any
 #if USE_FLD==0
-#if NVAR > NHYDRO+NENER
               do ivar = nhydro+1+nener, nvar
+#else
+              do ivar = nhydro+1+nener, nvar-1
+#endif
                  if(write_conservative) then
                     if (metal .and. imetal == ivar) then
                        field_name = 'metal_density'
@@ -195,27 +198,6 @@ subroutine backup_hydro(filename, filename_desc)
                  end if
                  call generic_dump(field_name, info_var_count, xdp, unit_out, dump_info_flag, unit_info)
               end do
-#endif
-#else
-#if NPSCAL>0
-              if(write_conservative) then
-                 do ivar=1,npscal-1 ! Write conservative passive scalars if any
-                    do i=1,ncache
-                       xdp(i)=uold(ind_grid(i)+iskip,firstindex_pscal+ivar)
-                    end do
-                    write(field_name, '("scalar_cons_", i0.2)') ivar
-                    call generic_dump(field_name, info_var_count, xdp, unit_out, dump_info_flag, unit_info)
-                 end do
-              else
-                 do ivar=1,npscal-1 ! Write passive scalars if any
-                    do i=1,ncache
-                       xdp(i)=uold(ind_grid(i)+iskip,firstindex_pscal+ivar)/max(uold(ind_grid(i)+iskip,1),smallr)
-                    end do
-                    write(field_name, '("scalar_", i0.2)') ivar
-                    call generic_dump(field_name, info_var_count, xdp, unit_out, dump_info_flag, unit_info)
-                 end do
-              endif
-#endif
 #endif
 
 #if USE_FLD==1
